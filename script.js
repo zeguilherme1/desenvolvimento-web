32155fcc4a0dfdc6ad9f4bdd3ff80785752e0924
Leticia Adriana dos Santos <leticiaadrianas@gmail.com>
2025-09-11 16:50:01 -0300

    [ADD] pop-ups to a new branch

---
+++ script.js
@@ -0,0 +1,4 @@
+const openBtn = document.getElementById("openPop-history")
+const closeBtn = document.getElementById("closePop-history")
+const popup = document.getElementsByClassName("pop-up-history")
+
